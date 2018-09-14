bd70683ea673d568f300f82aee7354ec920b4fce
oceanly020 <oceanly020@gmail.com>
2018-09-14 22:42:31 +0800

    test link

---
+++ net_plumber/Ubuntu-NetPlumber-Release/i2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./net_plumber --load /home/azrael/workplace/hassel-public/hsa-python/examples/i2/i2_json_rules --policy /home/azrael/workplace/hassel-public/hsa-python/examples/i2/policy_all.json --hdr-len 16 
+# ./net_plumber --load /home/azrael/workplace/hassel-public/hsa-python/examples/i2/i2_json_rules --hdr-len 16
